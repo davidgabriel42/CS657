0b4a613003f3aa4efddbba067084ec6596ccf437
David Gabriel <contact@david-gabriel.com>
2019-05-05 16:18:28 -0700

    compiles. output uncertain. sparse array write large

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <tiledb/tiledb>
+#include <ctime>
 
 // Name of array.
-std::string array_name("quickstart_dense_array");
+std::string array_name("quickstart_sparse_array");
 
 void create_array() {
   // Create a TileDB context.
@@ -9,11 +10,11 @@ void create_array() {
 
   // The array will be 4x4 with dimensions "rows" and "cols", with domain [1,4].
   tiledb::Domain domain(ctx);
-  domain.add_dimension(tiledb::Dimension::create<int>(ctx, "rows", {{1, 4}}, 4))
-        .add_dimension(tiledb::Dimension::create<int>(ctx, "cols", {{1, 4}}, 4));
+  domain.add_dimension(tiledb::Dimension::create<int>(ctx, "rows", {{1, 125000000}},  1000))
+        .add_dimension(tiledb::Dimension::create<int>(ctx, "cols", {{1, 16}},  4));
 
   // The array will be dense.
-  tiledb::ArraySchema schema(ctx, TILEDB_DENSE);
+  tiledb::ArraySchema schema(ctx, TILEDB_SPARSE);
   schema.set_domain(domain)
         .set_order({{TILEDB_ROW_MAJOR, TILEDB_ROW_MAJOR}});
 
@@ -24,6 +25,32 @@ void create_array() {
   tiledb::Array::create(array_name, schema);
 }
 
+void write_array() {
+  tiledb::Context ctx;
+
+  // Prepare some data for the array
+ int n =  2000000000;
+ int val = 1;
+ std::vector<int32_t> data;
+ data.assign(n,val);
+
+  // Open the array for writing and create the query.
+  tiledb::Array array(ctx, array_name, TILEDB_WRITE);
+  tiledb::Query query(ctx, array);
+  query.set_layout(TILEDB_UNORDERED)
+       .set_buffer("a", data);
+
+  // Perform the write and close the array.
+  query.submit();
+  array.close();
+}
+
 int main(){
+
+time_t tstart, tend; 
+ tstart = time(0);
 create_array();
+write_array();
+tend = time(0); 
+std::cout << "It took "<< difftime(tend, tstart) <<" second(s)."<< std::endl;
 }

39f35d26eae89d5fb6868dc50b3a94669d62ddaf
David Gabriel <contact@david-gabriel.com>
2019-05-05 17:48:32 -0700

    results and rand write

--- main.cpp
+++ main.cpp
@@ -2,16 +2,19 @@
 #include <ctime>
 #include <iostream>
 
+/*
+changed to input operands for all funcs
 //global benchmark vars
-int n =  100000000;
+int base = 1;
+int n =  20000000;
 int val = 1;
 int nrows = 16;
 int ncols =  125000000;
-
+*/
 // Name of array.
 std::string array_name("quickstart_sparse_array");
 
-void create_array() {
+void create_array(int base, int n, int val, int nrows, int ncols) {
   // Create a TileDB context.
   tiledb::Context ctx;
 
@@ -32,7 +35,7 @@ void create_array() {
   tiledb::Array::create(array_name, schema);
 }
 
-void write_array() {
+void write_array(int base, int n, int val, int nrows, int ncols) {
   tiledb::Context ctx;
 
   // Prepare some data for the array
@@ -42,7 +45,7 @@ data.assign(n,val);
 
 std::vector<int> coords;
 
-int k = 1;
+int k = base;
 for(int i = 1; i <= n/nrows; i++)
 {
 
@@ -67,16 +70,48 @@ k++;
 }
 
 int main(){
+int base = 1;
+int n =  20000000;
+int val = 1;
+int nrows = 16;
+int ncols =  125000000;
 
-time_t tstart, tend; 
- tstart = time(0);
-create_array();
-write_array();
-tend = time(0); 
+time_t tstart, t1, t2, t3, t4, tend;
 
 std::cout << "nrows= "<< std::to_string(nrows) << std::endl;
 std::cout << "ncols= "<< std::to_string(ncols) << std::endl;
 std::cout << "ndata= "<< std::to_string(n) << std::endl;
 
-std::cout << "It took "<< difftime(tend, tstart) <<" second(s)."<< std::endl;
+tstart = time(0);
+
+
+create_array(base,n,val,nrows,ncols);
+t1 = time(0);
+std::cout << "Array initialization took  "<< difftime(t1, tstart) <<" second(s)."<< std::endl;
+
+write_array(base,n,val,nrows,ncols);
+t2 = time(0);
+std::cout << "First write block took  "<< difftime(t2, t1) <<" second(s)."<< std::endl;
+
+base += n;
+n += n;
+write_array(base,n,val,nrows,ncols);
+t3 = time(0);
+std::cout << "Second write block took "<< difftime(t3, t2) <<" second(s)."<< std::endl;
+
+
+base += n;
+n += n;
+write_array(base,n,val,nrows,ncols);
+t4 = time(0);
+std::cout << "Third write block took "<< difftime(t4, t3) <<" second(s)."<< std::endl;
+
+base += n;
+n += n;
+write_array(base,n,val,nrows,ncols);
+tend = time(0);
+
+std::cout << "Fourth write block took "<< difftime(tend, t4) <<" second(s)."<< std::endl;
+std::cout << "Total time was  "<< difftime(tend, tstart) <<" second(s)."<< std::endl;
+
 }
